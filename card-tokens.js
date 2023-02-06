a7af4d092ebd66b7835c3141358150674e1421ae
xaukael <37848032+xaukael@users.noreply.github.com>
2023-02-05 21:21:04 -0600

    Add files via upload

--- card-tokens.js
+++ card-tokens.js
@@ -4,18 +4,31 @@ var createCardToken = async function(data, userId) {
   let src = data.src;
   let actor = game.actors.find(a=>a.flags.world?.card==card.uuid);
   if (actor) {
-    let scene = game.scenes.get(data.scene)
+    //let scene = game.scenes.get(data.scene)
     //if (canvas.scene.id!=data.scene) return ui.notifications.warn('A player is trying to spawn a card on another scene');
-    if (scene.tokens.find(t=>t.actor.flags.world?.card==data.uuid)) return ui.notifications.warn('Card already on canvas');
+    //if (scene.tokens.find(t=>t.actor.flags.world?.card==data.uuid)) return ui.notifications.warn('Card already on canvas');
     warpgate.spawnAt(data, await actor.getTokenDocument(), {token:{texture:{src}}})
     return; 
   }
   Hooks.once('createActor', async (actor)=>{
-    let scene = game.scenes.get(data.scene)
-    if (scene.tokens.find(t=>t.actor?.flags?.world?.card==data.uuid)) return ui.notifications.warn('Card already on canvas');
+    //let scene = game.scenes.get(data.scene)
+    //if (scene.tokens.find(t=>t.actor?.flags?.world?.card==data.uuid)) return ui.notifications.warn('Card already on canvas');
     warpgate.spawnAt(data, await actor.getTokenDocument(), {token:{texture:{src}}})
   })
 }
+
+Hooks.on('preCreateToken', (token)=>{
+  if (!token.actor.flags.world?.card) return;
+  let scenes = game.scenes.filter(m=>m.tokens.filter(i=>i.actor?.id==token.actor.id).length);
+  if (!scenes.length) {
+    let card = fromUuidSync(token.actor.flags.world?.card)
+    let src = card.face?card.faces[card.face].img:card.back.img;
+    token.data.update({texture:{src}});
+    return true;
+  }
+  ui.notifications.warn(`Card already on scenes: ${scenes.map(s=>s.name).join()}`);
+  return false;
+})
 
 Hooks.once("socketlib.ready", () => {
 	window.socket = socketlib.registerModule("card-tokens");
@@ -66,10 +79,12 @@ Hooks.on('renderTokenHUD', (app, html, hudData)=>{
   if (!card) return;
   html.find('.control-icon').remove();
   html.find('.attribute.bar1').addClass('flexrow').css({'justify-content': 'center', 'top': '1em'})
+  if (card.back.img!=app.object.document.texture.src)
   html.find('.attribute.bar1').append($(`<div class="control-icon" style="margin:.2em; width:${app.object.w*2}px; flex: 0 0 ${app.object.w/2}px;" title="back"><center>
   <img style="padding:0.1em; width:${card.width*canvas.grid.size/4}px; height:${card.height*canvas.grid.size/4}px;" src="${card.back.img}"></center></div>`)
   .click(function(){ card.update({face:null}) }))
   for (let face of card.faces) {
+    if (face.img!=app.object.document.texture.src)
     html.find('.attribute.bar1').append($(`<div class="control-icon" data-face="${card.faces.indexOf(face)}" style="margin:.2em; width:${app.object.w*2}px; flex: 0 0 ${app.object.w/2}px;" title="${face.name}"><center>
     <img style="padding:0.1em; width:${card.width*canvas.grid.size/4}px; height:${card.height*canvas.grid.size/4}px;" src="${face.img}"></center></div>`)
     .click(function(){ card.update({face:Number($(this).data().face)}) }))
@@ -87,12 +102,9 @@ Hooks.on('renderTokenHUD', (app, html, hudData)=>{
 Hooks.on('deleteActor', async (actor)=>{
   if (!game.user.isGM) return;
   if (!actor.flags.world?.card) return;
-  let tokens = actor.getActiveTokens();
-  await Promise.all(tokens.map(async t=> { return await t.scene.deleteEmbeddedDocuments("Token", [t.document.id])}))
-  return true;
+  //for (let scene of game.scenes) scene.deleteEmbeddedDocuments("Token", scene.tokens.filter(i=>i.actor?.id==actor.id).map(t=>t._id))
+  await Promise.all(game.scenes.map(s=>{return s.deleteEmbeddedDocuments("Token", s.tokens.filter(i=>i.actor?.id==actor.id).map(t=>t._id))}))
 })
-
-
 
 Hooks.on('createCard', async (card, options, user)=>{
   if (!game.user.isGM) return;
@@ -124,9 +136,10 @@ Hooks.on('updateCard', async (card, update, options, user)=>{
   if (!update.hasOwnProperty('face')) return;
   let actor = game.actors.find(a=>a.flags.world?.card==card.uuid);
   if (!actor) return;
-  let tokens = actor.getActiveTokens();
+  //let tokens = actor.getActiveTokens();
   let src = card.faces[update.face]?.img || card.back.img;
-  await Promise.all(tokens.map(async t=> { return await t.scene.updateEmbeddedDocuments("Token", [{_id: t.document.id, texture:{src}}])}))
+  await Promise.all(game.scenes.map(s=> { return  s.updateEmbeddedDocuments("Token", s.tokens.filter(i=>i.actor?.id==actor.id).map(t=>{return {_id:t._id, texture:{src}}}))}))
+  //await Promise.all(tokens.map(async t=> { return await t.scene.updateEmbeddedDocuments("Token", [{_id: t.document.id, texture:{src}}])}))
 })
 
 Hooks.on('deleteCard', async (card, options, user)=>{
